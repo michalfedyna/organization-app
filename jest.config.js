520fd50a9115c5cb4ea05f7669a8c7154cdb0ef2
michalfedyna <michal@fedyna.pl>
2023-10-11 02:44:28 +0200

    chore: fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,12 @@
+/** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
   preset: 'react-native',
+  rootDir: '.',
+  resetMocks: true,
+  testMatch: ['<rootDir>/tests/**/*.ts?(x)', '<rootDir>/src/**/*.test.ts?(x)'],
   setupFilesAfterEnv: [
     '@testing-library/jest-native/extend-expect',
     './jestSetup.js',
   ],
-  testTimeout: 30000,
+  testTimeout: 60000,
 };
